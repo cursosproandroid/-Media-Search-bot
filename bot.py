bb28ac1f0c3a3dd48727108761fe6952bad42a8f
CursosProAndroid <90671027+cursosproandroid@users.noreply.github.com>
2021-09-16 00:33:16 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+import logging
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.ERROR)
+
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from utils import Media
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            session_name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
+    async def start(self):
+        await super().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        self.username = '@' + me.username
+        print(f"{me.first_name} hecho con Pyrogram v{__version__} (Capa {layer}) iniciado en {me.username}.")
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot se detuvo. Adiós.")
+
+
+app = Bot()
+app.run()
